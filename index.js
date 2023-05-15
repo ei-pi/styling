250dc69891addb45ddd0b863bf25914f3047cc8a
ei-pi <91853103+ei-pi@users.noreply.github.com>
2023-05-15 15:07:52 -0400

    draft 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.querySelectorAll("h2, h3, h4").forEach(heading => {
-    const url = `#${heading.id = heading.textContent.replace(/ /g, "-")}`;
+document.querySelectorAll("h1, h2, h3").forEach(heading => {
+    const url = `#${heading.id ||= heading.textContent.replace(/ /g, "-")}`;
     heading.addEventListener("click", () => window.location.href = url);
 });
